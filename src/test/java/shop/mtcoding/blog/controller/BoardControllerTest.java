136f1ce207c2ae55471edeccdcd275879600cedc
kim ho hyeon <khh5762@naver.com>
2023-02-07 10:31:30 +0900

    게시글 목록보기 통합 test 완료

--- src/test/java/shop/mtcoding/blog/controller/BoardControllerTest.java
+++ src/test/java/shop/mtcoding/blog/controller/BoardControllerTest.java
@@ -1,11 +1,16 @@
 package shop.mtcoding.blog.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +22,9 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.blog.dto.board.BoardResp;
 import shop.mtcoding.blog.model.User;
 
 @AutoConfigureMockMvc
@@ -27,6 +35,9 @@ public class BoardControllerTest {
     private MockMvc mvc;
 
     private MockHttpSession mockSession;
+
+    @Autowired
+    private ObjectMapper om;
 
     @BeforeEach // Test 메서드 실행 직전 마다에 호출됨.
     public void setUp() {
@@ -39,6 +50,27 @@ public class BoardControllerTest {
 
         mockSession = new MockHttpSession();
         mockSession.setAttribute("principal", user);
+    }
+
+    @Test
+    public void main_test() throws Exception {
+        // given
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/"));
+        Map<String, Object> map = resultActions.andReturn().getModelAndView().getModel();
+        List<BoardResp.BoardMainRespDto> dtos = (List<BoardResp.BoardMainRespDto>) map.get("dtos");
+        System.out.println("테스트 : size : " + dtos.size());
+        String model = om.writeValueAsString(dtos);
+        System.out.println("테스트 : size : " + model);
+
+        // then
+        resultActions.andExpectAll(status().isOk());
+        Assertions.assertThat(status().isOk());
+        assertThat(dtos.size()).isEqualTo(6);
+        assertThat(dtos.get(0).getUsername()).isEqualTo("ssar");
+        assertThat(dtos.get(0).getTitle()).isEqualTo("첫 번째 글입니다.");
+
     }
 
     @Test
